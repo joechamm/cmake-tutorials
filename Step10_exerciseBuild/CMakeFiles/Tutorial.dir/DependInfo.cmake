f9eeb91b96ae1a007753cc870e8c0df56347f116
Joseph Cunningham <joechamm@outlook.com>
2020-09-13 01:57:31 -0400

    Fix MathFunctions/CMakeLists.txt (only needed to add tutorial_compiler_flags to target_link_libraries)

---
+++ Step10_exerciseBuild/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/srv/samba/private/development/cmake-tutorials/Step10_exercise/tutorial.cxx" "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/CMakeFiles/Tutorial.dir/tutorial.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/srv/samba/private/development/cmake-tutorials/Step10_exercise/MathFunctions"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/MathFunctions/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Step10_exerciseBuild/CTestTestfile.cmake
@@ -0,0 +1,25 @@
+# CMake generated Testfile for 
+# Source directory: /srv/samba/private/development/cmake-tutorials/Step10_exercise
+# Build directory: /srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Runs "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "25")
+set_tests_properties(Runs PROPERTIES  _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;51;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Usage "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial")
+set_tests_properties(Usage PROPERTIES  PASS_REGULAR_EXPRESSION "Usage:.*number" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;54;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp4 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "4")
+set_tests_properties(Comp4 PROPERTIES  PASS_REGULAR_EXPRESSION "4 is 2" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;68;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp9 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "9")
+set_tests_properties(Comp9 PROPERTIES  PASS_REGULAR_EXPRESSION "9 is 3" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;69;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp5 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "5")
+set_tests_properties(Comp5 PROPERTIES  PASS_REGULAR_EXPRESSION "5 is 2.236" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;70;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp7 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "7")
+set_tests_properties(Comp7 PROPERTIES  PASS_REGULAR_EXPRESSION "7 is 2.645" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;71;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp25 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "25")
+set_tests_properties(Comp25 PROPERTIES  PASS_REGULAR_EXPRESSION "25 is 5" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;72;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp-25 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "-25")
+set_tests_properties(Comp-25 PROPERTIES  PASS_REGULAR_EXPRESSION "-25 is [-nan|nan|0]" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;73;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+add_test(Comp0.0001 "/srv/samba/private/development/cmake-tutorials/Step10_exerciseBuild/Tutorial" "0.0001")
+set_tests_properties(Comp0.0001 PROPERTIES  PASS_REGULAR_EXPRESSION "0.0001 is 0.01" _BACKTRACE_TRIPLES "/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;61;add_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;74;do_test;/srv/samba/private/development/cmake-tutorials/Step10_exercise/CMakeLists.txt;0;")
+subdirs("MathFunctions")
