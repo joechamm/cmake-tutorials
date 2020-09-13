73f5d9f6397ee56d07b457b995cf1a24f38a30d1
Joseph Cunningham <joechamm@outlook.com>
2020-09-13 03:36:11 -0400

    finish Step12

---
+++ Step12/release/MathFunctions/CMakeFiles/MathFunctions.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../libMathFunctions.pdb"
+  "../libMathFunctions.so"
+  "../libMathFunctions.so.1"
+  "../libMathFunctions.so.1.0.0"
+  "CMakeFiles/MathFunctions.dir/MathFunctions.cxx.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunctions.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
